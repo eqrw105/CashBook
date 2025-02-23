b066253a68bc9cb77b81c0f96b347353d3a0c93b
eqrw105 <eqrw105@naver.com>
2025-02-23 17:53:03 +0900

    feat: systemBar 색상 지정

--- composeApp/src/androidMain/kotlin/com/nims/cashbook/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nims/cashbook/MainActivity.kt
@@ -3,15 +3,23 @@ package com.nims.cashbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsControllerCompat
 import com.nims.cashbook.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+            val isDarkTheme = isSystemInDarkTheme()
+            WindowInsetsControllerCompat(window, window.decorView).apply {
+                isAppearanceLightStatusBars = !isDarkTheme
+                isAppearanceLightNavigationBars = !isDarkTheme
+            }
             App()
         }
     }
