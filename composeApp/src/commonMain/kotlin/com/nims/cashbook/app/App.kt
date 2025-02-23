23959d9ec01c50297a4a96ca9ef8483262ca0686
eqrw105 <eqrw105@naver.com>
2025-02-23 17:53:03 +0900

    feat: systemBar 색상 지정

--- composeApp/src/commonMain/kotlin/com/nims/cashbook/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/nims/cashbook/app/App.kt
@@ -1,12 +1,25 @@
 package com.nims.cashbook.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.nims.cashbook.core.designsystem.CashBookTheme
 import com.nims.cashbook.feature.main.MainScreen
 
 @Composable
 fun App() {
     CashBookTheme {
-        MainScreen()
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(CashBookTheme.colors.background)
+                    .safeDrawingPadding(),
+        ) {
+            MainScreen()
+        }
     }
 }
